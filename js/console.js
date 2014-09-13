7f5cf7f2b8240e134c3adff6c80b9f501f78e163
Derek Liang <frostyrusty@gmail.com>
2014-09-12 17:42:19 -0700

    updated command calling w/ function callbacks w/ mapping, new help command that lists out all commands (maybe adding secret commands later on)

--- js/console.js
+++ js/console.js
@@ -25,6 +25,36 @@
      inputText_[0].scrollIntoView();
  }
 
+var commands = [];
+ 
+commands["about"] = function (args) {
+	 printHtml("consol3 based on " + '<a href=\"http://www.htmlfivewow.com/demos/terminal/terminal.html\">HTML5 Terminal</a>');
+};
+
+commands["clear"] = function (args) {
+	clearConsole(inputText_);
+};
+
+commands["cd"] = function (args) {
+	window.location.href = args[0];
+};
+
+commands["date"] = function (args) {
+	 printHtml((new Date()).toLocaleString());
+};
+
+var tabularCommandPrint = "<table>";
+tabularCommandPrint += "<tr><td>" + 	"ABOUT" 	+ "</td>" + "<td>" + "Displays about the console" + "</td></tr>";
+tabularCommandPrint += "<tr><td>" + 	"CLEAR" 	+ "</td>" + "<td>" + "Clears the console" + "</td></tr>";
+tabularCommandPrint += "<tr><td>" + 	"CD" 		+ "</td>" + "<td>" + "Changes directory to the target" + "</td></tr>";
+tabularCommandPrint += "<tr><td>" + 	"DATE" 		+ "</td>" + "<td>" + "Displays the current date on this machine" + "</td></tr>";
+tabularCommandPrint += "</table>";
+
+commands["help"] = function (args) {
+	 printHtml(tabularCommandPrint);
+};
+
+
  function processCommand(e) {
      /* Ignore empty input and BACKSPACES */
      if (!inputText_.val() && e.keyCode == 8) {
@@ -50,20 +80,15 @@
              var cmd = args[0].toLowerCase();
              args = args.splice(1);
          }
-
-         /* COMMANDS */
-         switch (cmd) {
-             case 'clear':
-                 clearConsole(inputText_);
-                 return;
-             case 'date':
-                 printHtml((new Date()).toLocaleString());
-                 break;
-             default:
-                 if (cmd) {
-                     printHtml(cmd + ': command not found');
-                 }
-         };
+		
+		 var callback = commands[cmd];
+		 
+		 if (typeof callback == 'undefined') { // command does not exist
+         	printHtml(cmd + ': command not found');
+		 }
+		 else {
+			 callback(args);
+		 }
 
          inputText_.val('');
      }
